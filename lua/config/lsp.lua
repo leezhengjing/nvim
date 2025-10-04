3497a52623ee1955d177da7a45e68960b8180b55
leezhengjing <zhengjing.lee86@gmail.com>
2025-10-05 01:49:59 +0800

    feat: add copilot

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -37,6 +37,18 @@ vim.diagnostic.config({
 		border = "rounded",
 		source = true,
 	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+		},
+	},
 })
 
 vim.lsp.enable({
