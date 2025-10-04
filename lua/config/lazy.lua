c24b66bcb63a520265b10d54e1638815afd6225c
leezhengjing <zhengjing.lee86@gmail.com>
2025-10-04 16:56:50 +0800

    chore: migrate plugins to lua/plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,73 +15,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = false,
-        priority = 1000,
+require("lazy").setup({
+    spec = {
+        {import="plugins"},
     },
-
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        branch = 'master',
-        lazy = false,
-        build = ":TSUpdate"
-    },
-    { 'nvim-treesitter/playground' },
-    { 'theprimeagen/harpoon' },
-    { 'tpope/vim-fugitive' },
-    { 'mbbill/undotree' },
-
-    { 'neovim/nvim-lspconfig' },
-
-    { 'nvim-mini/mini.nvim',       version = "*" },
-    { 'akinsho/toggleterm.nvim',   version = "*", config = true },
-    {
-        'davidmh/mdx.nvim',
-        config = true,
-        dependencies = { "nvim-treesitter/nvim-treesitter" }
-    },
-    {
-      'sainnhe/sonokai',
-      lazy = false,
-      priority = 1000,
-      config = function()
-        -- Optionally configure and load the colorscheme
-        -- directly inside the plugin declaration.
-        vim.g.sonokai_enable_italic = true
-        vim.cmd.colorscheme('sonokai')
-      end
-    },
-{
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
-}
-}
-
-require("lazy").setup(plugins, {
     change_detection = { notify = false },
     install = {},
     checker = { enabled = true },
