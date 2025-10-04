dfdd4330de140e3e956f149b7c6d91d9802903ce
leezhengjing <zhengjing.lee86@gmail.com>
2025-10-04 17:23:35 +0800

    chore: format

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,21 +1,21 @@
 return {
-    cmd = {
-        'clangd',
-        '--clang-tidy',
-        '--background-index',
-        '--offset-encoding=utf-8',
-        '--compile-commands-dir=.',
-        '--header-insertion=iwyu',
-    },
-    root_markers = { '.clangd', 'compile_commands.json', '.git', '.clang_format' },
-    filetypes = { "c", "cpp", "objc", "objcpp" },
-    capabilities = {
-        textDocument = {
-            completion = {
-                completionItem = {
-                    snippetSupport = true,
-                }
-            }
-        }
-    }
+	cmd = {
+		"clangd",
+		"--clang-tidy",
+		"--background-index",
+		"--offset-encoding=utf-8",
+		"--compile-commands-dir=.",
+		"--header-insertion=iwyu",
+	},
+	root_markers = { ".clangd", "compile_commands.json", ".git", ".clang_format" },
+	filetypes = { "c", "cpp", "objc", "objcpp" },
+	capabilities = {
+		textDocument = {
+			completion = {
+				completionItem = {
+					snippetSupport = true,
+				},
+			},
+		},
+	},
 }
