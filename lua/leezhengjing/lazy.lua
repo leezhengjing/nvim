03ca08841099dc5282dec92c3a8ef1e2f41e360e
leezhengjing <zhengjing.lee86@gmail.com>
2025-10-04 16:32:16 +0800

    chore: add colorscheme and lazygit

--- lua/leezhengjing/lazy.lua
+++ lua/leezhengjing/lazy.lua
@@ -47,7 +47,38 @@ local plugins = {
         'davidmh/mdx.nvim',
         config = true,
         dependencies = { "nvim-treesitter/nvim-treesitter" }
+    },
+    {
+      'sainnhe/sonokai',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.sonokai_enable_italic = true
+        vim.cmd.colorscheme('sonokai')
+      end
+    },
+{
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
+}
 }
 
 require("lazy").setup(plugins, {
--- lua/leezhengjing/remap.lua
+++ lua/leezhengjing/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.keymap.set("n", "x", '"_x')
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -33,3 +34,21 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "te", ":tabedit")
+vim.keymap.set("n", "<tab>", ":tabnext<Return>")
+vim.keymap.set("n", "<s-tab>", ":tabprev<Return>")
+vim.keymap.set("n", "tw", ":tabclose<Return>")
+
+vim.keymap.set("n", "ss", ":split<Return>")
+vim.keymap.set("n", "sv", ":vsplit<Return>")
+
+vim.keymap.set("n", "sh", "<C-w>h")
+vim.keymap.set("n", "sk", "<C-w>k")
+vim.keymap.set("n", "sj", "<C-w>j")
+vim.keymap.set("n", "s", "<C-w>l")
+
+vim.keymap.set("n", "<C-S-h>", "<C-w><")
+vim.keymap.set("n", "<C-S-l>", "<C-w>>")
+vim.keymap.set("n", "<C-S-k>", "<C-w>+")
+vim.keymap.set("n", "<C-S-j>", "<C-w>-")
