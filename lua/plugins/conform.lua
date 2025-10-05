9e2b652ce66bf34b9a7327d326e4918c7511af0a
leezhengjing <zhengjing.lee86@gmail.com>
2025-10-05 17:55:51 +0800

    chore: add conform script

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,4 +5,19 @@ return {
 			python = { "black" },
 		},
 	},
+	keys = {
+		{
+			"<leader>f",
+			function()
+				local conform = require("conform")
+				if conform and conform.format then
+					conform.format({ async = true })
+				else
+					vim.lsp.buf.format({ async = true })
+				end
+			end,
+			desc = "Format buffer (Conform or LSP fallback)",
+			mode = { "n", "v" },
+		},
+	},
 }
